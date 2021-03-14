f03bddafe6a6687df33c3aa12b010f5b795da9fd
kobux-dev <kobuzisalex@hotmail.com>
2021-03-15 01:15:34 +0530

    cause wither jenkins script is stupid and its starting an eng build with no reason, or i am

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -8,6 +8,5 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/descendant_miatoll.mk
 
 COMMON_LUNCH_CHOICES := \
-    descendant_miatoll-user \
     descendant_miatoll-userdebug \
-    descendant_miatoll-eng
+
